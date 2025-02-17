8504802a43adb3c59d7aaa0a8a33bd8d1330fe02
rtocastro <ricktorres@live.com>
2025-02-16 18:53:04 -0800

    corrected app.jsx file  and installed dependecies for bootstrap and react

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-<h1>THIS IS THE MAIN PAGE</h1>
+<h1>THIS IS THE APP PAGE</h1>
     </>
   )
 }
