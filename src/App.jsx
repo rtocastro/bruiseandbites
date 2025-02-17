ad319c770febba717fad51274d0cc4cda5178898
rtocastro <ricktorres@live.com>
2025-02-17 15:58:37 -0800

    playing around with css lt/drk themes and made logo responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { Outlet } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { useState } from 'react'
 import './App.css'
+
+//Main pic
+import mainlogo from './assets/mainweblogopb.png'
 
 function App() {
 
   return (
     <>
+    {/* adding image to for link later */}
+<img src={mainlogo} />
 <h1>THIS IS THE APP PAGE</h1>
 <Outlet />
     </>
