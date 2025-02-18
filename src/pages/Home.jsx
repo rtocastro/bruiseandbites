cf518b4442e083d97a9134c869fdb404d64f2f9f
rtocastro <ricktorres@live.com>
2025-02-17 18:53:04 -0800

    added new folder for pages and created home landing page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link }  from 'react-router-dom'
+
+//images for menus etc
+
+export default function Home(){
+    <>
+    <h2>this is the landing for homepage</h2>
+    </>
+}
