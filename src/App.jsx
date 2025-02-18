d5beaaf2239b5d440a384ff0af9e9594ac8a9d7b
rtocastro <ricktorres@live.com>
2025-02-18 08:06:03 -0800

    created footer/header and change appjsx to hide h/f upon entry

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import './App.css'
+import React from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
+import './App.css';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
+  const location = useLocation();
+
+  // Hide Header and Footer when on the root path "/"
+  const isRoot = location.pathname === '/';
 
   return (
     <>
+      {!isRoot && <Header />}
       <Outlet />
+      {!isRoot && <Footer />}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+
+  return (
+    <>
+this is the Footer 
+    </>
+  )
+}
+
+export default Footer
