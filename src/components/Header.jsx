d119f3ba1a7e8a3513c3dec576d9c51b5d3a9861
rtocastro <ricktorres@live.com>
2025-02-19 00:54:37 -0800

    added image files for Headerjsx modified css for images

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import ordernow from '../assets/ordernowbutt.png';
+import homebutt from '../assets/homebutt.png';
+// import app from '../App'
 
 function Header() {
-
   return (
-    <>
-this is the header 
-    </>
-  )
+    <header className="header-container">
+      <div className="header-left">
+        <Link to="/">
+          <img src={homebutt} alt="Home" className="header-btn" />
+        </Link>
+      </div>
+      -center
+      <div className="header-right">
+        <Link to="/">
+          <img src={ordernow} alt="Order Now" className="header-btn" />
+        </Link>
+      </div>
+
+    </header>
+  );
 }
 
-export default Header
+export default Header;
