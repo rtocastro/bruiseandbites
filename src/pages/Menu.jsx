ee8e81181784014187b2781b1daa9fb4d2063a0e
rtocastro <ricktorres@live.com>
2025-02-21 13:47:38 -0800

    uploaded/imported images to menu page

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import commimg from '../assets/communityimg.png';
+
+import brewspic from '../assets/brewspic.png'
+import sandwichpic from '../assets/sandwichpic.png'
 
 
 function Menu() {
   return (
-    <><h1>this is the menu page</h1></>
+    <><h1>this is the menu page</h1>
+    
+    <img src={brewspic} />
+    <br />
+    <img src={sandwichpic} />
+    </>
   );
 }
 
