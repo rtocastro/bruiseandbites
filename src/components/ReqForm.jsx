60f5e48d67b7dcd8f0766806b2725b9bf17e0963
rtocastro <ricktorres@live.com>
2025-02-26 08:20:36 -0800

    playing with styling/hover for button on request page

--- src/components/ReqForm.jsx
+++ src/components/ReqForm.jsx
@@ -67,7 +67,7 @@ function ReqForm() {
       </label>
       <br />
       <br />
-      <button type="submit">Submit</button>
+      <button type="submit" backgroundColor='blue' >Submit</button>
       <br />
       <br />
     </form>
