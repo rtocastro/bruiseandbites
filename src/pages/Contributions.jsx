ca6e69a3f2184845c7cf0ce0297bd1ea154891dd
rtocastro <ricktorres@live.com>
2025-02-25 23:22:42 -0800

    created page to put contributors on

---
+++ src/pages/Contributions.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+
+
+function Contributions() {
+
+  return (
+    <>
+<h1>Contributions</h1>
+<p>
+Contributions for art for website through thenounproject:
+
+Nibblings World
+https://www.instagram.com/nibblingsworld/
+
+Andre Luiz
+https://thenounproject.com/creator/aluizgollo/
+
+Edy Subiyanto
+https://thenounproject.com/creator/jaruka/
+
+Paul Terrel
+https://thenounproject.com/creator/pterrelldesign/
+
+Aditya Chan
+https://thenounproject.com/creator/aditya_chan/
+
+Evan Bond
+https://thenounproject.com/creator/evan103/
+
+Trimanggolo Mulyo
+https://thenounproject.com/creator/bakulicon/
+
+Alex Muravev
+https://thenounproject.com/creator/alex2900/
+
+Rank Sol
+https://thenounproject.com/creator/ranksol/
+
+Cindy Clegane
+https://thenounproject.com/creator/cindyclegane/
+
+Nikita Kozin
+https://thenounproject.com/creator/kozinn/
+
+Emily Bernal
+https://instagram.com/em.ontheinternet
+https://unsplash.com/@emilybernal
+
+Tri Hartono
+https://thenounproject.com/creator/onomailbox/
+
+
+
+Free Fair & Healthy
+https://thenounproject.com/creator/freefairhealthy
+
+</p>
+
+    </>
+  )
+}
+
+export default Contributions
