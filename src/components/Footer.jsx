3e3c3e33b9b885e9d8f1a0f4b9da3d6d1968bff2
rtocastro <ricktorres@live.com>
2026-05-11 21:15:39 -0700

    cleaned up footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,23 +1,27 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-import iglogo from '../assets/igfootlogo.png'
+import igLogo from "../assets/igfootlogo.png";
 
 function Footer() {
-
   return (
-    <>
-    <Link >
-    <img src={iglogo} className='footer-btn'/>
-    </Link>
-    <p>follow us on IG!</p>
-    <br />
-    <br />
-    <Link to="/Contributions">
-    Contributions
-    </Link>
-    </>
-  )
+    <footer className="footer-container">
+      <a
+        href="https://instagram.com/"
+        target="_blank"
+        rel="noreferrer"
+        className="footer-social-link"
+      >
+        <img src={igLogo} className="footer-btn" alt="Instagram" />
+      </a>
+
+      <p>Follow us on IG!</p>
+
+      <Link to="/contact" className="footer-link">
+        Questions? Contact PB&apos;s
+      </Link>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
